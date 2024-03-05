450e05f5ac4b1d6b11e53ae695fbcd42e5fb6fb3
TMRXTechnology <Rafael.medeiros@outlook.de>
2024-03-04 23:11:03 -0300

    chore: update jest.config.ts to use jest-preset-angular for TypeScript file transformation

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  globals: { 
+  },
+  moduleNameMapper: { 
+  }, 
+};
+
+export default config;
